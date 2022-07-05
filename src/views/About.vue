77c45190fc894ed4ad4d2c9b9b0d43011ed3b171
biancabuzatu2000 <95959789+biancabuzatu2000@users.noreply.github.com>
2022-07-05 12:48:45 +0300

    my first project

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="about">
+        <h1> About me </h1>
+        <p>Hi! I am Bianca</p>
+        <p>
+            "I owe my success to having listened respectfully to the very best advice, and then going away and doing the exact opposite."
+                 -G. K. Chesterton
+        </p>
+    </main>
+
+</template>
+<style lang="scss">
+.about {
+    .p {
+        text-align: center;
+    }
+}
+</style>
