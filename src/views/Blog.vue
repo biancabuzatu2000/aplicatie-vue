d2ec3d2d6b8b69b3a79ec6fc73d59f73be5a44ad
biancabuzatu2000 <95959789+biancabuzatu2000@users.noreply.github.com>
2022-07-05 13:57:18 +0300

    Update Blog.vue

---
+++ src/views/Blog.vue
@@ -0,0 +1,40 @@
+<template>
+    <main class="blog">
+        <h1>Blog</h1>
+        <ul>
+            <li v-for="post in posts">
+                <h2>
+                    <span>
+                        {{ post.id }}.&nbsp;
+                    </span>
+                    <router-link :to="`/post/${post.id}`"> {{ post.title }} </router-link>
+                </h2>
+                <p>
+                    {{ post.body }}
+                </p>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        name: 'Blog',
+        data () {
+            return {
+                posts: []
+            }
+        },
+        created() {
+            const url= 'https://jsonplaceholder.typicode.com/posts'
+            axios.get(url)
+            .then(resp => {
+                this.posts = resp.data
+            })
+            .catch(e => {
+                console.log(e.response ? e.response.data :e)
+            })
+        }
+    }
+</script>
