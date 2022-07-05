46c1672d77a206478d3ee958deee986b30f52db1
biancabuzatu2000 <95959789+biancabuzatu2000@users.noreply.github.com>
2022-07-05 14:11:36 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+   <MainHeader>
+
+   </MainHeader>
     <router-view/>
   </div>
 </template>
 
+<script>
+    import MainHeader  from "./components/MainHeader"
+    export default {
+        name: 'App',
+        components: {
+            MainHeader
+        }
+    }
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  color: #ff8080;
   text-align: center;
-  color: #2c3e50;
+  font-family: "Times New Roman";
 }
 
 nav {
-  padding: 30px;
+  padding: 40px;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    font-weight: normal;
+     color: #ff8080;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #ff0000;
     }
   }
+}
+li {
+    margin-left: 20px;
+    list-style-type: none;
+}
+p {
+   margin-left: 20px;
+   max-width: 600px;
 }
 </style>
