7d3921a0cdcf722047cfd1ac681967af26052cf5
biancabuzatu2000 <95959789+biancabuzatu2000@users.noreply.github.com>
2022-07-05 13:58:22 +0300

    Update Contact.vue

---
+++ src/views/Contact.vue
@@ -0,0 +1,60 @@
+<template>
+    <main class="contact-page">
+        <h1>Contactează-ne: </h1>
+        <form @submit.prevent="handleSubmit">
+            <label for="name">
+                name:
+                <input id="name" text="text" >
+            </label>
+            <label for="msg">
+            message:
+            <textarea id="msg" />
+            </label>
+            <button>
+                Trimite mesajul
+            </button>
+        </form>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'Contact',
+        methods: {
+            handleSubmit () {
+                console.log('send detail to server')
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .contact-page {
+        form {
+            
+            margin: 0 auto;
+            text-align: left;
+            width: 300px;
+            border-radius: 17px;
+
+        label {
+            display: block;
+            }
+        input, textarea {
+            display: block;
+            width: 100%;
+            resize: none;
+            }
+        button {
+            padding: 11px 20px;
+            border-radius: 7px;
+            display: block;
+            margin-left: auto;
+            color: white;
+            margin-top: 20px;
+            background-color: #ffb3b3;
+            border: none;
+            }
+        }
+    }
+</style>
